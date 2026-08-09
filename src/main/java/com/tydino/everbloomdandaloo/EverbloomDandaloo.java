c39033bd80bb9c2cc916202cd16d79b81cf707c9
tydino <tydino909@gmail.com>
2026-08-09 11:53:44 -0400

    TODO adjust on 8/9/26
    I just added more things to the TODO

--- src/main/java/com/tydino/everbloomdandaloo/EverbloomDandaloo.java
+++ src/main/java/com/tydino/everbloomdandaloo/EverbloomDandaloo.java
@@ -22,9 +22,12 @@ import java.util.List;
 
 /// TODO:
 /// Fix the menu for the Fossil Recombinator
+/// Implement the menu to the Fossil Adapter
 /// Add Motabolism to the dinosaurs
 /// Fix textures of the custom blocks
+/// Make the fossil Adapter functionality
 /// Make the egg only hatchable around specific dinosaurs/mobs, or can be put into an incubator.
+/// Make and implement the missing textures and ways to get the untextured items
 
 /// REMINDERS
 /// Texture filter;monochromatic at 25% on multiply
