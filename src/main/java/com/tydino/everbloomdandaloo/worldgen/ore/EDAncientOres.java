9b33976486a43a709c631ef702bbc164adc1d459
tydino <tydino909@gmail.com>
2026-08-15 21:38:22 -0400

    amber ore is also now gettable
    Just a quick port over of the brachiosaurus to be under amber instead
    
    I also removed the configured feature resource key as I have no use for it at this time.

--- src/main/java/com/tydino/everbloomdandaloo/worldgen/ore/EDAncientOres.java
+++ src/main/java/com/tydino/everbloomdandaloo/worldgen/ore/EDAncientOres.java
@@ -1,22 +1,29 @@
 package com.tydino.everbloomdandaloo.worldgen.ore;
 
 import com.tydino.everbloomdandaloo.EverbloomDandaloo;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class EDAncientOres {
-    public static ResourceKey<ConfiguredFeature<?, ?>> configuredFeatureKey(String name) {
-        return ResourceKey.create(Registries.CONFIGURED_FEATURE, Identifier.fromNamespaceAndPath(EverbloomDandaloo.MOD_ID, name));
+
+    public static final ResourceKey<PlacedFeature> Amber = placedFeatureKey("amber");
+
+    public static void onInitialize(){
+        EDJurrassicFossils.onInitialize();
+
+        BiomeModifications.addFeature(
+                BiomeSelectors.foundInOverworld(),
+                GenerationStep.Decoration.UNDERGROUND_ORES,
+                Amber
+        );
     }
 
     public static ResourceKey<PlacedFeature> placedFeatureKey(String name) {
         return ResourceKey.create(Registries.PLACED_FEATURE, Identifier.fromNamespaceAndPath(EverbloomDandaloo.MOD_ID, name));
-    }
-
-    public static void onInitialize(){
-        EDJurrassicFossils.onInitialize();
     }
 }
--- src/main/java/com/tydino/everbloomdandaloo/worldgen/ore/EDJurrassicFossils.java
+++ src/main/java/com/tydino/everbloomdandaloo/worldgen/ore/EDJurrassicFossils.java
@@ -4,12 +4,9 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.GenerationStep;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class EDJurrassicFossils {
-    public static final ResourceKey<ConfiguredFeature<?, ?>> Brachiosaurus = EDAncientOres.configuredFeatureKey("brachiosaurus_fossil");
-
     public static final ResourceKey<PlacedFeature> BrachiosaurusPlaced = EDAncientOres.placedFeatureKey("brachiosaurus_fossil");
 
     public static void onInitialize(){
